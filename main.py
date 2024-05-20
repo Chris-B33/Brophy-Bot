82a2471400213758f7ba6f84a10a3e2f122298ec
Chris <brophy.c03@gmail.com>
2024-05-20 23:16:08 +0100

    Basic bot created

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import discord
+from discord.ext import commands
 import openai
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-class BrophyBot(discord.Client):
-    async def on_ready(self):
-        print("Loaded in.")
+bot = commands.Bot(command_prefix="&", intents=intents)
 
-    async def on_message(self):
-        print("new message")
+@bot.event
+async def on_ready():
+    print("----------------------------------------------")
+    print(f"Logged in as:\n{bot.user.name} \n{bot.user.id}")
+    print("----------------------------------------------")
 
-if __name__ == "__main__":
-    with open("token.env", "r") as file:
-        token = file.read()
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-    client = BrophyBot(intents=intents)
-    client.run(token)
+    print(f"{message.author}: {message.content}")
+    print("----------------------------------------------")
+    
+    await bot.process_commands(message)
+
+@bot.command()
+async def echo(ctx, arg):
+    await ctx.send(arg)
+
+with open("token.env", "r") as file:
+    token = file.read()
+
+bot.run(token)
