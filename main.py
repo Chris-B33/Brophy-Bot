248a33ef962bd0502804516b1776c506795180ca
Chris <brophy.c03@gmail.com>
2024-05-20 23:58:56 +0100

    Turns out OpenAI's API has to be paid for. Think I might give up on this for a bit.

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
+###############################
+#           Imports           #
+###############################
+
 import discord
 from discord.ext import commands
-import openai
+from openai import OpenAI
+
+
+###############################
+#       Initialization        #
+###############################
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="&", intents=intents)
+with open("tokens.env", "r") as file:
+    content = file.readlines()
+    discord_token = content[0]
+    openai_key = content[1]
 
-@bot.event
+chat_bot = OpenAI(api_key=openai_key)
+discord_bot = commands.Bot(command_prefix="&", intents=intents)
+
+
+###############################
+#            Events           #
+###############################
+
+@discord_bot.event
 async def on_ready():
     print("----------------------------------------------")
-    print(f"Logged in as:\n{bot.user.name} \n{bot.user.id}")
+    print(f"Logged in as:\n{discord_bot.user.name} \n{discord_bot.user.id}")
     print("----------------------------------------------")
 
-@bot.event
+@discord_bot.event
 async def on_message(message):
-    if message.author == bot.user:
+    mention = f'<@!{discord_bot.user.id}>'
+    if message.author == discord_bot.user or not discord_bot.user.mentioned_in(message):
         return
 
+    # Get message
     print(f"{message.author}: {message.content}")
     print("----------------------------------------------")
-    
-    await bot.process_commands(message)
 
-@bot.command()
-async def echo(ctx, arg):
-    await ctx.send(arg)
+    # Send to OpenAI
+    request = chat_bot.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are the Trash Man, a pro wrestler. You throw garbage around the arena."},
+            {"role": "user", "content": "".join(message.content.split(" ")[1::])}
+        ]
+    )
+    response = request.choices[0].message
 
-with open("token.env", "r") as file:
-    token = file.read()
+    # Handle response
+    print(f"{discord_bot.user}: {response}")
+    print("----------------------------------------------")
+    await message.channel.send(response)
 
-bot.run(token)
+
+###############################
+#             Main            #
+###############################
+
+if __name__ == "__main__":
+    discord_bot.run(discord_token)
